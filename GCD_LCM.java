2ab2fd3d78c1033b115aeb3a200e820b82eb969d
Harsh Singh <127043034+singhharsh77@users.noreply.github.com>
2025-08-29 11:14:40 +0000

    readme and GCD_LCM

---
+++ GCD_LCM.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter integers (a,b) ");
+    if(!sc.hasNextLong()){
+        System.out.print("Enter a Valid Input for a ");
+        return;
+    }
+    long a = sc.nextLong();
+
+    if(!sc.hasNextLong()){
+        System.out.print("Enter a Valid Input for b ");
+    }
+    }
+}
