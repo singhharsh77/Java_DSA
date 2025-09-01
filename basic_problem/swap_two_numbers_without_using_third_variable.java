28d5d1ccc1c35a119c12c94c62fb9edd15cc20a8
Harsh Singh <127043034+singhharsh77@users.noreply.github.com>
2025-09-01 11:23:49 +0000

    done

---
+++ basic_problem/swap_two_numbers_without_using_third_variable.java
@@ -0,0 +1,34 @@
+package basic_problem;
+import java.util.*;
+public class swap_two_numbers_without_using_third_variable {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of 'a' : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter Value of 'b' : ");
+        int b = sc.nextInt();
+
+        System.out.println("Original numbers "+a+" "+b);
+
+        System.out.println("Using addition and sub.");
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After Swapping "+a+" "+b);
+
+        System.out.println("Using bitwise operator ");
+
+
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+
+        System.out.println("After Swapping "+a+" "+b);
+        
+        
+
+    }
+}
