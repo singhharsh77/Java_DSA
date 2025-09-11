f946293f1af030500ff894ccb95f7c6b2346569a
Harsh Singh <127043034+singhharsh77@users.noreply.github.com>
2025-09-11 17:32:30 +0000

    done

---
+++ basic_problem/function/primeNo.java
@@ -0,0 +1,27 @@
+package function;
+import java.util.*;
+
+import javax.sound.midi.SysexMessage;
+public class primeNo {
+
+    public static boolean isprime(int n){
+        for(int i =2;i <= Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = sc.nextInt();
+
+        if(isprime(n)){
+            System.out.println("is a prime num: ");
+        }else{
+            System.out.println("not a prime: ");
+        }
+    }
+    
+}
