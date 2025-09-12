6edc0bc9c3ff91f0fd8c11fcfd9fdeab4ab55d38
Harsh Singh <127043034+singhharsh77@users.noreply.github.com>
2025-09-12 06:42:29 +0000

    done

---
+++ basic_problem/LongestSubstring.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LongestSubstring  {
+
+    public static int lengthOfLongestSubstring(String str){
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for(int right = 0; right<= str.length() -1 ;right++){
+            char c = str.charAt(right);
+
+            while(set.contains(c)){
+                set.remove(str.charAt(left));
+                left++;
+
+            }
+
+            set.add(c);
+            maxLen = Math.max(maxLen, right-left +1);
+                }
+                return maxLen;
+    } 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+
+        int result = lengthOfLongestSubstring(str);
+
+        System.out.println("Length of longest substring without repeating characters: " + result);
+    }    
+}
