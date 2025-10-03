2376a8353a53c8ccd107f97dbaa4299c85d8a46e
Harsh Singh <127043034+singhharsh77@users.noreply.github.com>
2025-10-03 13:33:13 +0000

    done

---
+++ Recursion_BackTracking/FindSubset.java
@@ -0,0 +1,27 @@
+package Recursion_BackTracking;
+import java.util.*;
+public class FindSubset {
+    public static void main(String[] args){
+        int[] arr = {1,2};
+        List<Integer> current = new ArrayList<>();
+        findSubset(arr,0,current);
+    }
+
+    public static void findSubset(int[] arr, int index ,List<Integer> current){
+
+        if(index == arr.length){
+            System.out.println(current);
+            return;
+        }
+
+    findSubset(arr, index+1, current);
+    current.add(arr[index]);
+
+    findSubset(arr, index+1, current);
+    current.remove(current.size() -1);
+
+
+    }
+}
+    
+
